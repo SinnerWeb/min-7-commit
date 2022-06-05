f9b1f758329a5a3c77fe1f01cc7c8685ee8a7106
SinnerWeb <Dwesh5030@gmail.com>
2022-06-05 18:55:52 +0300

    ready js

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+let name = prompt('Как тебя зовут ?');
+alert(`Привет ${name}`);
+let surName = prompt('Укажи фамилию это очень важно');
+let age = prompt('И свой возраст ;)')
+userSurName = surName;
+userAge = age;
+userAge = 23;
+alert(`И так ты ${name} ${userSurName} и тебе ${userAge} года.`);
